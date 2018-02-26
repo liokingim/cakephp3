2b458d8fb710fea60c9ccbe7eaf1eaff0a91f1b2
liokingim-PC\liokingim <liokingim@daum.net>
2018-02-27 00:11:45 +0900

    MenusController.php chronos

--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -7,10 +7,6 @@ use Cake\Datasource\Exception\RecordNotFoundException;
 use Cake\Chronos\Chronos;
 use Cake\Chronos\ChronosInterface;
 use Cake\Chronos\MutableDateTime;
-use function json_encode;
-use const JSON_PRETTY_PRINT;
-use function var_dump;
-use function debug;
 
 /**
  * Menus Controller
